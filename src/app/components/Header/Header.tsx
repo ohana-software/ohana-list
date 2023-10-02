372aa13feeafb43fe24d479dbf46be9a43e5b3d3
Rallenson <Rallenson900@gmail.com>
2023-10-02 11:01:01 -0300

    fix:title style

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -15,7 +15,13 @@ export default function Header() {
         position="relative"
         >
             <BtnTheme />
-            <Heading>
+            <Heading
+            bgGradient='linear(to right, #E9C347 42%, #D16F61 50%, #B32694 62%)'
+            bgClip='text'
+            fontSize={{ base: "24px", md: "42px", lg: "48px" }}
+            fontWeight='extrabold'
+            w="100vw"
+            >
                 Ohana List
             </Heading>
             <SearchBar />
