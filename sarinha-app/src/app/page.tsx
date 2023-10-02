3ba8db271116746b7f22f6035712e294fbd12ce9
Sarah Carla Souza <sarah.souza@foa.org.br>
2023-10-02 17:30:32 -0300

    Adiciona header - Cria a classe .title no globals.css - Adiciona o componente Center

--- sarinha-app/src/app/page.tsx
+++ sarinha-app/src/app/page.tsx
@@ -3,6 +3,10 @@ import { Center } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className='container'>app</main>
+    <main className='container'>
+        <Center bg='#D9D9D9'  h='200px'  fontSize='40' fontWeight={"900"} letterSpacing="-2.6px">
+          <h1 className='title'>Ohana List</h1>
+        </Center>
+    </main>
   )
 }
