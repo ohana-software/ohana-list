ade73556b3f25e33b6c589ebcfac06c40376d708
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-10-03 09:46:49 -0300

    Added button to create a task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Flex, Input, Text, useColorMode } from '@chakra-ui/react'
+import { Box, Button, Flex, Input, Text, useColorMode } from '@chakra-ui/react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -45,14 +45,13 @@ export default function Home() {
 
       {/* Body */}
       <Flex justify="center" bg="gray.600" h="80vh">
-        <Flex>
+        <Flex align="center" h="6vh" mt="-30px" gap="8px">
           <Input
             placeholder="Adicione uma tarefa"
             bg="gray.500"
-            h="6vh"
             _placeholder={{ color: 'gray.300' }}
             textColor="gray.100"
-            p="0.8rem"
+            p="1rem"
             w="35vw"
             _focus={{
               transition: '0.5s',
@@ -60,9 +59,20 @@ export default function Home() {
               borderColor: 'dark_pink',
             }}
             borderRadius="8px"
-            mt="-30px"
             zIndex="10"
           />
+          <Button
+            p="1rem"
+            bg="yellow_button"
+            borderRadius="8px"
+            w="6rem"
+            color="gray.100"
+            fontWeight="bold"
+            gap="8px"
+          >
+            Criar
+            <Image src="/plus.svg" height={16} width={16} alt="add task icon" />
+          </Button>
         </Flex>
       </Flex>
     </>
