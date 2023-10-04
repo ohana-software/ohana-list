72c690c1f8f6f12d9c1ca8150770fd0e25af66ce
Sarah <95034183+piltie@users.noreply.github.com>
2023-10-03 23:24:30 -0300

    refactor: Transfere a lógica de exibição da lista para o TaskList

--- sarinha-app/src/app/components/task/TaskList.tsx
+++ sarinha-app/src/app/components/task/TaskList.tsx
@@ -1,13 +1,16 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Center, Flex } from "@chakra-ui/react";
 import TaskDetail from "./TaskDetail";
 import Task from "../../models/Task"
+import ClipboardIcon from "../icons/ClipboardIcon";
 
 interface Props {
   tasks: Task[]
 }
 export default function TaskList({ tasks }: Props) {
-  return (
-    <Flex
+  let dashboard;
+  if (tasks.length > 0) {
+    dashboard = (
+      <Flex
       flexDirection='column'
       gap='14px'
     >
@@ -16,5 +19,94 @@ export default function TaskList({ tasks }: Props) {
         ) 
       }
     </Flex>
+    )
+  } else {
+    dashboard = (
+      <Center
+      textAlign='center'
+      p='64px 24px'
+      fontSize='16px'
+      color='#808080'
+      borderRadius='8px'
+      borderTop='1px'
+      borderColor='#333'
+    >
+      <Flex
+        flexDirection='column'
+        alignItems='center'
+        gap='16px'
+      >
+        <ClipboardIcon w="56px" h="56px"/>
+        <Box
+          as='p'
+          lineHeight='22.4px'
+        >
+          <Box
+            as='span'
+            fontWeight={'700'}
+          >
+            Você ainda não tem tarefas cadastradas
+          </Box>
+          <br/>
+          <Box
+            as='span'
+            fontWeight={'400'}
+          >
+            Crie tarefas e organize seus itens a fazer
+          </Box>
+        </Box>
+      </Flex>
+    </Center>
+    )
+  }
+
+  return (
+    <Flex
+    width='736px'
+    m='0 auto'
+    mt='64px'
+    gap='24px'
+    flexDirection='column'
+  >
+    <Flex
+      justifyContent='space-between'
+      width='100%'
+      fontSize='14px'
+      fontWeight={"700"}
+    >
+      <Flex
+        color='#D57B5A'
+        gap='8px'
+      >
+        Tarefas criadas
+        <Box
+          p='2px 8px'
+          fontSize='12px'
+          borderRadius='999px'
+          bg='#D9D9D9'
+          color='#333'
+        >
+          { tasks.length }
+        </Box>
+      </Flex>
+      <Flex
+        color='#BF477E'
+        gap='8px'
+      >
+        Concluídas
+        <Box
+          p='2px 8px'
+          fontSize='12px'
+          borderRadius='999px'
+          bg='#D9D9D9'
+          color='#333'
+        >
+          { tasks.filter(task => task.finished == true).length }
+        </Box>
+      </Flex>
+    </Flex>
+      {dashboard}
+  </Flex>
+
   )
 }
--- sarinha-app/src/app/page.tsx
+++ sarinha-app/src/app/page.tsx
@@ -25,51 +25,6 @@ export default function Home() {
     setNewTask('')
   }
 
-  let dashboard;
-  if (tasks.length > 0) {
-    dashboard = (
-      <TaskList tasks={tasks} />
-    )
-  } else {
-    dashboard = (
-      <Center
-      textAlign='center'
-      p='64px 24px'
-      fontSize='16px'
-      color='#808080'
-      borderRadius='8px'
-      borderTop='1px'
-      borderColor='#333'
-    >
-      <Flex
-        flexDirection='column'
-        alignItems='center'
-        gap='16px'
-      >
-        <ClipboardIcon w="56px" h="56px"/>
-        <Box
-          as='p'
-          lineHeight='22.4px'
-        >
-          <Box
-            as='span'
-            fontWeight={'700'}
-          >
-            Você ainda não tem tarefas cadastradas
-          </Box>
-          <br/>
-          <Box
-            as='span'
-            fontWeight={'400'}
-          >
-            Crie tarefas e organize seus itens a fazer
-          </Box>
-        </Box>
-      </Flex>
-    </Center>
-    )
-  }
-
   return (
     <main className='container'>
         <Center
@@ -124,52 +79,7 @@ export default function Home() {
           </Flex>
         </Flex>
 
-        <Flex
-          width='736px'
-          m='0 auto'
-          mt='64px'
-          gap='24px'
-          flexDirection='column'
-        >
-          <Flex
-            justifyContent='space-between'
-            width='100%'
-            fontSize='14px'
-            fontWeight={"700"}
-          >
-            <Flex
-              color='#D57B5A'
-              gap='8px'
-            >
-              Tarefas criadas
-              <Box
-                p='2px 8px'
-                fontSize='12px'
-                borderRadius='999px'
-                bg='#D9D9D9'
-                color='#333'
-              >
-                { tasks.length }
-              </Box>
-            </Flex>
-            <Flex
-              color='#BF477E'
-              gap='8px'
-            >
-              Concluídas
-              <Box
-                p='2px 8px'
-                fontSize='12px'
-                borderRadius='999px'
-                bg='#D9D9D9'
-                color='#333'
-              >
-                { tasks.filter(task => task.finished == true).length }
-              </Box>
-            </Flex>
-          </Flex>
-            {dashboard}
-        </Flex>
+        <TaskList tasks={tasks} />
     </main>
   )
 }
