4c24fd30b7f37236904dfa4c137352fc8ec8bc3a
Sarah Carla Souza <sarah.souza@foa.org.br>
2023-10-04 16:59:29 -0300

    Adiciona font weights no theme

--- sarinha-app/src/app/theme.ts
+++ sarinha-app/src/app/theme.ts
@@ -7,10 +7,10 @@ const breakpoints = {
 
 const colors = {
   product: {
-    "pink": "#BF477E",
+    pink: "#BF477E",
     "dark-pink": "#B22D95",
     "light-orange": "#D57B5A",
-    "yellow-button": "#ECB62A"
+    "yellow-button": "#ECB62A",
   },
   base: {
     gray: {
@@ -20,9 +20,9 @@ const colors = {
       400: "#333333",
       500: "#262626",
       600: "#1A1A1A",
-      700: "#0D0D0D"
+      700: "#0D0D0D",
     },
-    danger: "#E25858"
+    danger: "#E25858",
   },
   fontSizes: {
     xs: "12px",
@@ -30,9 +30,14 @@ const colors = {
     md: "16px",
     lg: "40px",
   },
+  fontWeights: {
+    regular: 400,
+    bold: 700,
+    black: 900,
+  },
   lineHeights: {
-    base: "140%"
-  }
-}
+    base: "140%",
+  },
+};
 
 export const theme = extendTheme({ breakpoints, colors });
